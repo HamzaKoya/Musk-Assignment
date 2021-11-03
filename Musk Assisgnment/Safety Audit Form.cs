066957589d96a73e6049fa1e20f3c7d56645ed24
HamzaKoya <ham151@student.aru.ac.uk>
2021-11-03 14:32:38 +0000

    GUI

---
+++ Musk Assisgnment/Safety Audit Form.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Musk_Assisgnment
+{
+    public partial class Safety_Audit_Form : Form
+    {
+        public Safety_Audit_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Safety_Audit_Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
